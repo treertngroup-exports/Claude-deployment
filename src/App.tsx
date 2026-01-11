e2a99f31d0a5ce8b80197764c1d82fe26604088f
treertngroup-exports <treertngroup@gmail.com>
2026-01-11 21:45:46 +0530

    Refactor App component and add ScrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ExportProcess from "./components/ExportProcess";
+import { useEffect } from "react";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
@@ -14,12 +14,12 @@ import Contact from './components/Contact';
 import WhatsAppButton from './components/WhatsAppButton';
 import GalleryPage from './pages/GalleryPage';
 
+// Home Page Composition
 function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-hidden">
       <Hero />
       <Stats />
-      <ExportProcess />
       <Services />
       <Gallery />
       <Certificates />
@@ -31,22 +31,38 @@ function Home() {
   );
 }
 
-function App() {
+// Scroll to top on route change (important for /gallery)
+function ScrollToTop() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior });
+  }, []);
+
+  return null;
+}
+
+export default function App() {
   return (
     <Router>
+      <ScrollToTop />
+
       <div className="min-h-screen flex flex-col relative">
+        {/* Navbar */}
         <Navbar />
+
+        {/* Main Content */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/gallery" element={<GalleryPage />} />
           </Routes>
         </main>
+
+        {/* Footer */}
         <Footer />
+
+        {/* Floating WhatsApp Button */}
         <WhatsAppButton />
       </div>
     </Router>
   );
 }
-
-export default App;
