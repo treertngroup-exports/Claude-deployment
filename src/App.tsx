aa0636e104c0faedbaece94e1259f23a01bbce70
treertngroup-exports <treertngroup@gmail.com>
2026-01-11 20:44:17 +0530

    Add ProcessParallax component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProcessParallax from "./components/ProcessParallax";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
@@ -18,6 +19,7 @@ function Home() {
     <div className="min-h-screen">
       <Hero />
       <Stats />
+      <ProcessParallax />
       <Services />
       <Gallery />
       <Certificates />
