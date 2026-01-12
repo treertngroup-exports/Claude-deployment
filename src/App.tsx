c70ddc78fdff0278ff19846a9944b62d90b1a553
treertngroup-exports <treertngroup@gmail.com>
2026-01-12 23:03:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import WhatsAppButton from "./components/WhatsAppButton";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
 import Services from "./components/Services";
-{/*import Gallery from "./components/Gallery";*/}
+//import Gallery from "./components/Gallery";
 import Certificates from "./components/Certificates";
 import Quality from "./components/Quality";
 import WhyChoose from "./components/WhyChoose";
