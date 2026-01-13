b32b68e6a70ec03ffc671bdbca4f0f89f06f14cf
treertngroup-exports <treertngroup@gmail.com>
2026-01-13 23:33:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import WhatsAppButton from "./components/WhatsAppButton";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
 import Services from "./components/Services";
-//import Gallery from "./components/Gallery";
+// import Gallery from "./components/Gallery";
 import Certificates from "./components/Certificates";
 import Quality from "./components/Quality";
 import WhyChoose from "./components/WhyChoose";
@@ -26,7 +26,7 @@ function Home() {
       <Hero />
       <Stats />
       <Services />
-       {/*<Gallery />*/}
+      {/* <Gallery /> */}
       <Certificates />
       <Quality />
       <WhyChoose />
