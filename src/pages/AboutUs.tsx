267027164d7a8d3e2cd89c15a1645f29841d9764
treertngroup-exports <treertngroup@gmail.com>
2026-01-22 22:10:10 +0530

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
-import { Linkedin, X } from "lucide-react";
+import { Linkedin, X, Target, Eye} from "lucide-react";
 
 /* =========================
    Types
