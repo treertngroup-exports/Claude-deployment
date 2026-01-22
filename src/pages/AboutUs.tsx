1dacddcce8cad14664e3f40eed2fd8c947ccc2c9
treertngroup-exports <treertngroup@gmail.com>
2026-01-22 22:45:24 +0530

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -244,13 +244,20 @@ function LeaderTile({
   return (
     <button onClick={() => onClick(leader)} className="group text-left w-full">
       <div
-        className={`relative w-full aspect-[4/3] rounded-2xl overflow-hidden ${leader.color} flex items-end justify-center pb-4`}
+        className={`relative w-full aspect-[4/3] rounded-2xl overflow-hidden ${leader.color} flex items-end justify-center`}
       >
-        <img
-          src={leader.image}
-          alt={leader.name}
-          className="h-[82%] w-auto object-contain"
-        />
+        {/* Safe inner frame */}
+        <div className="w-full h-full flex items-end justify-center pb-4">
+          <img
+            src={leader.image}
+            alt={leader.name}
+            className="max-h-[85%] max-w-[85%] object-contain"
+            onError={(e) => {
+              // Fallback if Google thumbnail fails
+              (e.currentTarget as HTMLImageElement).src = "/placeholder-person.png";
+            }}
+          />
+        </div>
       </div>
 
       <div className="mt-4">
@@ -262,5 +269,6 @@ function LeaderTile({
     </button>
   );
 }
+
 
 
