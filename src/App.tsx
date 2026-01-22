bda5a5acb4d1b646abc5e24dc3172a5344bb8bd9
treertngroup-exports <treertngroup@gmail.com>
2026-01-22 23:58:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
-
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 import GalleryPage from "./pages/GalleryPage";
 import AboutUs from "./pages/AboutUs";
-import ExportStory from "./components/ExportStory";
-import Contact from "./components/Contact";
 
-/* =========================
-   Home Page
-========================= */
-function Home() {
+export default function App() {
   return (
-    <div className="min-h-screen">
-      <ExportStory />
-      <Contact />
-    </div>
-  );
-}
-
-/* =========================
-   App Layout
-========================= */
-function AppLayout() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-
-      <main className="flex-grow">
+    <Router>
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/about-us" element={<AboutUs />} />
         </Routes>
-      </main>
-
-      <Footer />
-      <WhatsAppButton />
-    </div>
-  );
-}
-
-/* =========================
-   Root App
-========================= */
-export default function App() {
-  return (
-    <Router>
-      <AppLayout />
+      </Layout>
     </Router>
   );
 }
