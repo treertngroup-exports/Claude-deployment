1e2ee923810e52c262286484cb644a804ef07c82
treertngroup-exports <treertngroup@gmail.com>
2026-01-22 23:43:50 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import WhatsAppButton from "./WhatsAppButton";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      {/* This spacer pushes all content below the fixed navbar */}
-      <main className="pt-32">{children}</main>
+      {/* Push content below fixed navbar */}
+      <main className="pt-32 min-h-screen">{children}</main>
+      <Footer />
+      <WhatsAppButton />
     </>
   );
 }
