502464a63965af65e85ef93fb4f040f74d13b080
treertngroup-exports <treertngroup@gmail.com>
2026-01-23 23:49:22 +0530

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -250,7 +250,7 @@ function LeaderTile({
   onClick: (l: Leader) => void;
   variant: "founder" | "team";
 }) {
-  const imageHeight = variant === "founder" ? "h-[85%]" : "h-[97%]";
+  const imageHeight = variant === "founder" ? "h-[85%]" : "h-[85%]";
 
   // UK leaders
   const isUK =
