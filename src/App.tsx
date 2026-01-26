8af431dd5e9082cc351ed5da01da1d788fbdf7e3
treertngroup-exports <treertngroup@gmail.com>
2026-01-26 20:12:58 +0530

    Add ContactPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import GalleryPage from "./pages/GalleryPage";
 import AboutUs from "./pages/AboutUs";
 import Credibility from "./pages/Credibility";
-
+import ContactPage from "./pages/ContactPage"; // ✅ ADD THIS
 
 export default function App() {
   return (
@@ -15,6 +15,7 @@ export default function App() {
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/credibility" element={<Credibility />} />
+          <Route path="/contact" element={<ContactPage />} /> {/* ✅ ADD */}
         </Routes>
       </Layout>
     </Router>
